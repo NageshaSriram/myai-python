12306845e212a965869262340177801b95be11a1
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2024-09-01 00:02:52 +0530

    Added gen Ai dependency

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def greet():
-    print("Hello")
-greet()
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyCGfrszI0Hzxx8IhBiJMikiHDgWiTVG34g")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
